c176210a90f82a2b6bc02a01e4ba9d08d26bf6d0
RARodrigo11 <126535953+RARodrigo11@users.noreply.github.com>
2023-04-22 16:15:05 +0800

    Update index.js

--- src/firebaseConfig/index.js
+++ src/firebaseConfig/index.js
@@ -3,13 +3,14 @@ import { getDatabase } from "firebase/database";
 
   function StartFirebase(){
     const firebaseConfig = {
-        apiKey: "AIzaSyCaiJdm4XuKhVJFSk3R0FoHzcV7C-R3aRk",
-        authDomain: "activity-crud.firebaseapp.com",
-        projectId: "activity-crud",
-        storageBucket: "activity-crud.appspot.com",
-        messagingSenderId: "1002555062716",
-        appId: "1:1002555062716:web:186f73a50a5da378b313b9"
-      };
+      apiKey: "AIzaSyCaiJdm4XuKhVJFSk3R0FoHzcV7C-R3aRk",
+      authDomain: "activity-crud.firebaseapp.com",
+      databaseURL: "https://activity-crud-default-rtdb.firebaseio.com",
+      projectId: "activity-crud",
+      storageBucket: "activity-crud.appspot.com",
+      messagingSenderId: "1002555062716",
+      appId: "1:1002555062716:web:406119cc8f961348b313b9"
+    };
 
       // Initialized
       const app = initializeApp(firebaseConfig);
